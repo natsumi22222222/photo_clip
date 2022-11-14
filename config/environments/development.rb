404a6fa32fd785d9995bcc421fa87c1a74a16110
natsumi22222222 <110472548+natsumi22222222@users.noreply.github.com>
2022-11-14 07:04:31 +0000

    [Start]model controller view create, login

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,5 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+config.hosts << "1de328cb2cc7470aba2d89d1e334755a.vfs.cloud9.ap-northeast-1.amazonaws.com"
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+
+  devise_for :users,skip: [:passwords], controllers: {
+    registrations: "public/regstrations",
+    sessions: "public/sessions"
+  }
+
+  devise_for :admins,skip: [:registrations,:passwords], controllers: {
+    sessions: "admin/sessions"
+  }
+
+
+
 end
