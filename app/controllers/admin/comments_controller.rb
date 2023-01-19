c1f031fc83e398070269824edad20bf52760b947
natsumi22222222 <110472548+natsumi22222222@users.noreply.github.com>
2023-01-19 13:43:30 +0000

    [Fix]pagenation

--- app/controllers/admin/comments_controller.rb
+++ app/controllers/admin/comments_controller.rb
@@ -2,6 +2,7 @@ class Admin::CommentsController < ApplicationController
 
   def index
     @comments= Comment.all
+    @comments_page= Comment.all.page(params[:page]).per(10)
   end
 
   def destroy
