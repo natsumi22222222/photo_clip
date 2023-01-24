ce1241232107ddf7cc69ee4dd1dafb3d126bca77
natsumi22222222 <110472548+natsumi22222222@users.noreply.github.com>
2023-01-24 11:57:18 +0000

    [Add]Favorite

--- app/controllers/public/favorites_controller.rb
+++ app/controllers/public/favorites_controller.rb
@@ -1,17 +1,16 @@
 class Public::FavoritesController < ApplicationController
+   before_action :post_params
 
   def create
-    post = Post.find(params[:post_id])
-    favorite = current_user.favorites.new(post_id: post.id)
-    favorite.save
-    redirect_to request.referer
+    Favorite.create(user_id: current_user.id, post_id: params[:id])
   end
 
   def destroy
-    post = Post.find(params[:post_id])
-    favorite = current_user.favorites.find_by(post_id: post.id)
-    favorite.destroy
-    redirect_to request.referer
+   Favorite.find_by(user_id: current_user.id, post_id: params[:id]).destroy
+  end
+
+  def post_params
+    @post = Post.find(params[:id])
   end
 
 end
